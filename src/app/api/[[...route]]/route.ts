15aa6ee7dbd5e2ab092e8e5454a5e233974a2cc4
ecarry <lianshiliang93@gmail.com>
2024-11-16 00:57:52 +0800

    feat: auth & drizzle config

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,9 +1,29 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import {
+  AuthConfig,
+  authHandler,
+  initAuthConfig,
+  verifyAuth,
+} from "@hono/auth-js";
 
-export const runtime = "edge";
+import authConfig from "@/auth.config";
+
+// Revert to "edge" if planning on running on the edge
+export const runtime = "nodejs";
+
+function getAuthConfig(): AuthConfig {
+  return {
+    secret: process.env.AUTH_SECRET,
+    ...authConfig,
+  };
+}
 
 const app = new Hono().basePath("/api");
+
+app.use("*", initAuthConfig(getAuthConfig));
+app.use("/auth/*", authHandler());
+app.use("/hello", verifyAuth());
 
 app.get("/hello", (c) => {
   return c.json({
@@ -11,5 +31,13 @@ app.get("/hello", (c) => {
   });
 });
 
+app.get("/hello1", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
+
 export const GET = handle(app);
 export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
