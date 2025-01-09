f257c73e99398d32c8863525bcb3f6fd540a8a1a
ecarry <lianshiliang93@gmail.com>
2025-01-09 20:54:45 +0800

    chore: auth page

--- src/feature/auth/components/sign-in.tsx
+++ src/feature/auth/components/sign-in.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 // External dependencies
+import { z } from "zod";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { signIn } from "@/feature/auth/lib/auth-client";
@@ -20,79 +21,144 @@ import {
   CardDescription,
   CardFooter,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+// Internal dependencies - Hooks
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+});
 
 export default function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [loadingGithub, setLoadingGithub] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    await signIn.email({
+      ...values,
+      fetchOptions: {
+        onRequest: () => {
+          setLoading(true);
+        },
+        onResponse: () => {
+          setLoading(false);
+        },
+        onError: (ctx) => {
+          toast.error(ctx.error.message);
+        },
+      },
+    });
+  };
+
   return (
-    <Card className="max-w-md">
-      <CardHeader>
+    <Card className="w-full h-full p-8">
+      <CardHeader className="px-0 pt-0">
         <CardTitle className="text-lg md:text-xl">Sign In</CardTitle>
         <CardDescription className="text-xs md:text-sm">
           Enter your email below to login to your account
         </CardDescription>
       </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-              disabled={loading}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              value={email}
+      <CardContent className="px-0 pb-0">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      placeholder="m@example.com"
+                      type="email"
+                      disabled={loading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs" />
+                </FormItem>
+              )}
             />
-          </div>
 
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Password"
+                      disabled={loading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs" />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex items-center gap-2">
+              <Checkbox
+                id="remember"
+                disabled={loading}
+                onClick={() => {
+                  setRememberMe(!rememberMe);
+                }}
+              />
+              <Label htmlFor="remember" className="text-xs">
+                Remember me
+              </Label>
             </div>
 
-            <Input
-              id="password"
-              type="password"
-              placeholder="password"
-              autoComplete="password"
-              value={password}
-              disabled={loading}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-          <div className="flex items-center gap-2">
-            <Checkbox
-              id="remember"
-              disabled={loading}
-              onClick={() => {
-                setRememberMe(!rememberMe);
-              }}
-            />
-            <Label htmlFor="remember">Remember me</Label>
-          </div>
-
+            <Button type="submit" className="w-full" disabled={loading}>
+              {loading ? (
+                <Loader2 size={16} className="animate-spin" />
+              ) : (
+                "Login"
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div
+          className={cn(
+            "w-full gap-2 flex items-center mt-2",
+            "justify-between flex-col"
+          )}
+        >
           <Button
-            type="submit"
-            className="w-full"
-            disabled={loading}
+            variant="outline"
+            disabled={loadingGithub}
+            className={cn("w-full gap-2")}
             onClick={async () => {
-              await signIn.email({
-                email,
-                password,
+              await signIn.social({
+                provider: "github",
+                callbackURL: "/profile",
                 fetchOptions: {
-                  onRequest: () => {
-                    setLoading(true);
-                  },
                   onResponse: () => {
-                    setLoading(false);
+                    setLoadingGithub(false);
+                  },
+                  onRequest: () => {
+                    setLoadingGithub(true);
                   },
                   onError: (ctx) => {
                     toast.error(ctx.error.message);
@@ -101,45 +167,22 @@ export default function SignIn() {
               });
             }}
           >
-            {loading ? <Loader2 size={16} className="animate-spin" /> : "Login"}
-          </Button>
-
-          <div
-            className={cn(
-              "w-full gap-2 flex items-center",
-              "justify-between flex-col"
-            )}
-          >
-            <Button
-              variant="outline"
-              disabled={loading}
-              className={cn("w-full gap-2")}
-              onClick={async () => {
-                await signIn.social({
-                  provider: "github",
-                  callbackURL: "/profile",
-                });
-              }}
-            >
+            {loadingGithub ? (
+              <Loader2 size={16} className="animate-spin" />
+            ) : (
               <Github size={16} />
-              Sign in with Github
-            </Button>
-          </div>
+            )}
+            Continue with Github
+          </Button>
         </div>
-
+      </CardContent>
+      <CardFooter className="px-0 py-0">
         <div className="flex justify-center w-full border-t py-4">
           <p className="text-center text-xs text-neutral-500">
             Don&apos;t have an account?{" "}
             <a href="/sign-up" className="text-orange-400">
               Sign Up
             </a>
-          </p>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <div className="flex justify-center w-full border-t py-4">
-          <p className="text-center text-xs text-neutral-500">
-            Secured by <span className="text-orange-400">better-auth.</span>
           </p>
         </div>
       </CardFooter>
