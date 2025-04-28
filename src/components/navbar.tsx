efe222c473c865fbba6d86495c6bb28765f09917
ecarry <lianshiliang93@gmail.com>
2025-04-28 07:21:54 +0000

    chore: user button component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,7 @@
 // External dependencies
 import Link from "next/link";
+import { headers } from "next/headers";
+import { auth } from "@/modules/auth/lib/auth";
 
 // Internal dependencies - UI Components
 import FlipLink from "./flip-link";
@@ -7,7 +9,11 @@ import { TbBrandNextjs } from "react-icons/tb";
 import { ThemeToggle } from "./theme-toggle";
 import UserButton from "@/modules/auth/components/user-button";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
   return (
     <div className="max-w-7xl mx-auto h-16 w-full flex items-center px-4">
       <Link href="/" className="flex items-center gap-2">
@@ -24,7 +30,7 @@ const Navbar = () => {
 
       <div className="ml-auto flex items-center gap-4">
         <ThemeToggle />
-        <UserButton />
+        <UserButton session={session} />
       </div>
     </div>
   );
--- src/modules/auth/components/user-button.tsx
+++ src/modules/auth/components/user-button.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { signOut, useSession } from "@/modules/auth/lib/auth-client";
+import { signOut } from "@/modules/auth/lib/auth-client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
@@ -13,68 +13,69 @@ import {
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { LogOut, Loader2 } from "lucide-react";
-import { Skeleton } from "@/components/ui/skeleton";
+import { Session } from "../lib/auth-types";
 
-const UserButton = () => {
+interface Props {
+  session: Session | null
+}
+
+const UserButton = ({
+  session
+}: Props) => {
   const router = useRouter();
   const [isSignOut, setIsSignOut] = useState<boolean>(false);
-  const { data: session, isPending } = useSession();
 
-  if (isPending) {
-    return <Skeleton className="size-9 rounded-full" />;
+  if(!session) {
+    return (
+      <Button asChild>
+          <Link href="/sign-in">Sign In</Link>
+        </Button>
+    )
   }
 
   return (
-    <>
-      {session ? (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Avatar className="size-9">
-              <AvatarImage
-                src={session?.user.image || "#"}
-                alt="Avatar"
-                className="object-cover"
-              />
-              <AvatarFallback>{session?.user.name.charAt(0)}</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem asChild>
-              <button
-                className="w-full"
-                onClick={async () => {
-                  setIsSignOut(true);
-                  await signOut({
-                    fetchOptions: {
-                      onSuccess() {
-                        router.push("/");
-                      },
-                    },
-                  });
-                  setIsSignOut(false);
-                }}
-                disabled={isSignOut}
-              >
-                <span className="text-sm">
-                  {isSignOut ? (
-                    <Loader2 size={15} className="animate-spin" />
-                  ) : (
-                    <div className="flex items-center gap-2">
-                      <LogOut size={16} />
-                      Sign Out
-                    </div>
-                  )}
-                </span>
-              </button>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      ) : (
-        <Button asChild>
-          <Link href="/sign-in">Sign In</Link>
-        </Button>
-      )}
-    </>
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="size-9">
+          <AvatarImage
+            src={session?.user.image || "#"}
+            alt="Avatar"
+            className="object-cover"
+          />
+          <AvatarFallback>{session?.user.name.charAt(0)}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem asChild>
+          <button
+            className="w-full"
+            onClick={async () => {
+              setIsSignOut(true);
+              await signOut({
+                fetchOptions: {
+                  onSuccess() {
+                    router.push("/");
+                  },
+                },
+              });
+              setIsSignOut(false);
+            }}
+            disabled={isSignOut}
+          >
+            <span className="text-sm">
+              {isSignOut ? (
+                <Loader2 size={15} className="animate-spin" />
+              ) : (
+                <div className="flex items-center gap-2">
+                  <LogOut size={16} />
+                  Sign Out
+                </div>
+              )}
+            </span>
+          </button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
