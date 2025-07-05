95ca4998f43417e5ae3d7f29afc571a4c86535d1
ecarry <lianshiliang93@gmail.com>
2025-07-05 14:58:41 +0000

    feat: implement dropdown menu for user avatar with account options

--- src/modules/home/ui/components/user-button.tsx
+++ src/modules/home/ui/components/user-button.tsx
@@ -1,27 +1,91 @@
+"use client";
+
+import {
+  IconCreditCard,
+  IconLogout,
+  IconNotification,
+  IconUserCircle,
+} from "@tabler/icons-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { authClient } from "@/modules/auth/lib/auth-client";
 import { Button } from "@/components/ui/button";
-import { UserAvatar } from "@/components/user-avatar";
-import { auth } from "@/modules/auth/lib/auth";
-import { headers } from "next/headers";
 import Link from "next/link";
 
-export const UserButton = async () => {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+export function UserButton() {
+  const { data: session } = authClient.useSession();
+
+  const user = {
+    name: session?.user?.name,
+    email: session?.user?.email,
+    avatar: session?.user?.image,
+  };
 
   return (
     <>
       {!!session ? (
-        <UserAvatar
-          size="lg"
-          imageUrl={session.user.image!}
-          name={session.user.name!}
-        />
+        <DropdownMenu modal={false}>
+          <DropdownMenuTrigger asChild>
+            <Avatar>
+              <AvatarImage src={user.avatar || ""} alt={user.name} />
+              <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+            side="bottom"
+            align="start"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage src={user.avatar || ""} alt={user.name} />
+                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="text-muted-foreground truncate text-xs">
+                    {user.email}
+                  </span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <IconUserCircle />
+                Account
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <IconCreditCard />
+                Billing
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <IconNotification />
+                Notifications
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => authClient.signOut()}>
+              <IconLogout />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
         <Button asChild>
-          <Link href="/sign-in">Sign In</Link>
+          <Link href="/sign-in">Sign in</Link>
         </Button>
       )}
     </>
   );
-};
+}
