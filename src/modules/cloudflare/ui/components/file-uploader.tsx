532d57ac1e657d1831fa1dcb39acaf2df0e95e7a
ecarry <lianshiliang93@gmail.com>
2025-07-09 12:57:10 +0000

    feat: add toast error messages and restrict file uploader to images only

--- src/modules/cloudflare/ui/components/file-uploader.tsx
+++ src/modules/cloudflare/ui/components/file-uploader.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback } from "react";
 import { FileRejection, useDropzone } from "react-dropzone";
+import { toast } from "sonner";
 
 const FileUploader = () => {
   const onDrop = useCallback((acceptedFiles: File[]) => {
@@ -10,8 +11,23 @@ const FileUploader = () => {
   }, []);
 
   const onDropRejected = useCallback((fileRejections: FileRejection[]) => {
-    // Do something with the rejected files
-    console.log(fileRejections);
+    if (fileRejections.length > 0) {
+      const tooManyFiles = fileRejections.find(
+        (fileRejection) => fileRejection.errors[0].code === "too-many-files"
+      );
+
+      const fileInvalidType = fileRejections.find(
+        (fileRejection) => fileRejection.errors[0].code === "file-invalid-type"
+      );
+
+      if (tooManyFiles) {
+        toast.error("Too many files");
+      }
+
+      if (fileInvalidType) {
+        toast.error("File type is not supported");
+      }
+    }
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -28,11 +44,11 @@ const FileUploader = () => {
       {...getRootProps()}
       className="border border-dashed border-gray-300 p-4 cursor-pointer hover:bg-gray-100 transition-colors flex items-center justify-center duration-200 rounded-md"
     >
-      <input {...getInputProps()} />
+      <input {...getInputProps()} accept="image/*" />
       {isDragActive ? (
-        <p>Drop the files here ...</p>
+        <p>Drop the file here ...</p>
       ) : (
-        <p>Drag &apos;n&apos; drop some files here, or click to select files</p>
+        <p>Drag &apos;n&apos; drop some file here, or click to select file</p>
       )}
     </div>
   );
